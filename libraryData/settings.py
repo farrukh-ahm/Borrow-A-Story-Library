bc59cf2061bfb4935a0520ddc55634fccfe5450c
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2024-06-26 14:49:49 +0000

    change allowed hosts

--- libraryData/settings.py
+++ libraryData/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['borrow-a-story.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['borrowastory.up.railway.app/']
 
 
 # Application definition
